28b540f936ad88e1d3c70bd5528b8b2d88b9532f
Paula Sturmer <paulasturmer@gmail.com>
2020-05-22 15:06:28 -0300

    Rails simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+4.times do
+    flat = Flat.create!(
+      name: Faker::Company.name,
+      address: Faker::Address.street_address,
+      description: Faker::Company.catch_phrase,
+      price_per_night: rand(50..200),
+      number_of_guests: rand(2..10)
+      )
+      puts "Flat #{flat.name} is created."
+    end
+    puts 'Seeding completed!'
